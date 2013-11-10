4e503d5f5add11f7af39b471ae83ae9f09b429db
Phillipp Röll <phillipp.roell@trafficplex.de>
2013-11-10 21:55:50 +0100

    remove URI parsing

--- lib/domainshare/apifunc.rb
+++ lib/domainshare/apifunc.rb
@@ -1,7 +1,7 @@
 module Domainshare
     class APIFUNC
         def initialize( name , args = [], opt_args = [])
-            @url = "https://api.domainshare.tk" + "/" + name
+            @name = name
             @args = args
             @opt_args = opt_args
         end
@@ -23,19 +23,17 @@ module Domainshare
         end
 
         def set_form_data(req, params, sep = '&')
-              req.body = params.map {|k, v| encode_kvpair(req, k, v) }.flatten.join(sep)
-        req.content_type = 'application/x-www-form-urlencoded'
+            req.body = params.map {|k, v| encode_kvpair(req, k, v) }.flatten.join(sep)
+            req.content_type = 'application/x-www-form-urlencoded'
         end
       
         def call( params = {}, type = "xml" )
-            url = URI.parse([ @url, type ].join("."))
-            http = Net::HTTP.new(url.host, url.port)
+            http = Net::HTTP.new("api.domainshare.tk", 443)
             http.use_ssl = true
-#           Un-comment the following line to debug the api calls and responses.
-#           http.set_debug_output $stderr
             http.verify_mode = OpenSSL::SSL::VERIFY_NONE if http.use_ssl?
-            req = Net::HTTP::Post.new(url.path)
-            self.set_form_data( req, params, ';')
+
+            req = Net::HTTP::Post.new("/" + @name + "." + type)
+            set_form_data( req, params, ';')
             resp = http.start {|http| http.request(req) }
             resp.body
         end
